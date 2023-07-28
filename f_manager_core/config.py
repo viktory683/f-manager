08b57c6585af60157691179a6df5c438ed1164ff
bzglve <viktory683@gmail.com>
2023-07-28 16:01:52 +0600

    config class refactor - move and rename section classes outside of main `Config` class - add base `ConfigSection` class for initialization and `as_dict` methods - use `Path().mkdir()` method insead of `os.makedirs()` - more clear and readeable `Config` class by splitting `__init__` into smaller methods

--- f_manager_core/config.py
+++ f_manager_core/config.py
@@ -1,7 +1,6 @@
 import configparser
 from functools import cached_property
 import json
-import os
 from pathlib import Path
 import platform
 import shutil
@@ -11,158 +10,188 @@ from f_manager_core.exceptions import GameNotFoundError, UnknownSystem
 from f_manager_core.helpers import expand_path
 
 
+class ConfigSection:
+    """Base class for configuration sections."""
+
+    def __init__(self, data: dict) -> None:
+        self._data = data
+
+    def as_dict(self) -> dict:
+        """Return the section data as a dictionary."""
+        exclude_keys = ["_data"]
+        return {
+            key: value
+            for key, value in self.__dict__.items()
+            if key not in exclude_keys
+        }
+
+
+class FactorioConfigSection(ConfigSection):
+    """Class for the Factorio configuration section."""
+
+    @cached_property
+    def username(self) -> Optional[str]:
+        """Return the Factorio username."""
+        if username := self._data.get("username"):
+            return str(username)
+
+        with self.dir.joinpath("player-data.json").open() as f:
+            player_data = json.load(f)
+
+        if username := player_data.get("service-username"):
+            return str(username)
+
+    @cached_property
+    def password(self) -> Optional[str]:
+        """Return the Factorio password."""
+        if password := self._data.get("password"):
+            return str(password)
+
+    @cached_property
+    def token(self) -> Optional[str]:
+        """Return the Factorio token."""
+        if token := self._data.get("token"):
+            return str(token)
+
+        with self.dir.joinpath("player-data.json").open() as f:
+            player_data = json.load(f)
+
+        if token := player_data.get("service-token"):
+            return str(token)
+
+    @cached_property
+    def dir(self) -> Path:
+        """Return the Factorio directory."""
+        path = expand_path(self._data.get("dir"))
+        try:
+            exe_file = list(list(path.joinpath("bin").iterdir())[0].iterdir())[0]
+            executable = shutil.which(exe_file)
+            if not executable:
+                raise GameNotFoundError(
+                    f"Could not find game executable file {exe_file}"
+                )
+        except FileNotFoundError as e:
+            raise GameNotFoundError("Could not find game executable file") from e
+        return path
+
+    @cached_property
+    def mods_dir(self) -> Path:
+        """Return the Factorio mods directory."""
+        if path := self._data.get("mods_dir"):
+            return expand_path(path)
+
+        return self.dir.joinpath("mods")
+
+    @cached_property
+    def data_dir(self) -> Path:
+        """Return the Factorio data directory."""
+        return self.dir.joinpath("data")
+
+
+class DataConfigSection(ConfigSection):
+    """Class for the data configuration section."""
+
+    @cached_property
+    def storage(self) -> Path:
+        """Return the data storage directory."""
+        path = expand_path(self._data.get("storage"))
+        path.mkdir(parents=True, exist_ok=True)
+        return path
+
+
+class LoggingConfigSection(ConfigSection):
+    """Class for the logging configuration section."""
+
+    @cached_property
+    def level(self) -> str:
+        """Return the logging level."""
+        return str(self._data.get("level"))
+
+    @cached_property
+    def path(self) -> Path:
+        """Return the logging path."""
+        return expand_path(self._data.get("path"))
+
+
 class Config:
-    class __Factorio:
-        def __init__(self, data: dict) -> None:
-            self.__data = data
-
-        @cached_property
-        def username(self) -> Optional[str]:
-            if username := self.__data.get("username"):
-                return str(username)
-
-            with self.dir.joinpath("player-data.json").open() as f:
-                player_data = json.load(f)
-
-            if username := player_data.get("service-username"):
-                return str(username)
-
-        @cached_property
-        def password(self) -> Optional[str]:
-            if password := self.__data.get("password"):
-                return str(password)
-
-        @cached_property
-        def token(self) -> Optional[str]:
-            if token := self.__data.get("token"):
-                return str(token)
-
-            with self.dir.joinpath("player-data.json").open() as f:
-                player_data = json.load(f)
-
-            if token := player_data.get("service-token"):
-                return str(token)
-
-        @cached_property
-        def dir(self) -> Path:
-            path = expand_path(self.__data.get("dir"))
-            try:
-                exe_file = list(list(path.joinpath("bin").iterdir())[0].iterdir())[0]
-                executable = shutil.which(exe_file)
-                if not executable:
-                    raise GameNotFoundError(
-                        f"Could not find game executable file {exe_file}"
-                    )
-            except FileNotFoundError as e:
-                raise GameNotFoundError("Could not find game executable file") from e
-            return path
-
-        @cached_property
-        def mods_dir(self) -> Path:
-            if path := self.__data.get("mods_dir"):
-                return expand_path(path)
-
-            return self.dir.joinpath("mods")
-
-        @cached_property
-        def data_dir(self) -> Path:
-            return self.dir.joinpath("data")
-
-        def as_dict(self) -> dict:
-            exclude_keys = ["_Factorio__data"]
-            return {
-                key: value
-                for key, value in self.__dict__.items()
-                if key not in exclude_keys
-            }
-
-    class __Data:
-        def __init__(self, data: dict) -> None:
-            self.__data = data
-
-        @cached_property
-        def storage(self) -> Path:
-            path = expand_path(self.__data.get("storage"))
-            os.makedirs(path, exist_ok=True)
-            return path
-
-        def as_dict(self) -> dict:
-            exclude_keys = ["_Data__data"]
-            return {
-                key: value
-                for key, value in self.__dict__.items()
-                if key not in exclude_keys
-            }
-
-    class __Logging:
-        def __init__(self, data: dict) -> None:
-            self.__data = data
-
-        @cached_property
-        def level(self) -> str:
-            return str(self.__data.get("level"))
-
-        @cached_property
-        def path(self) -> Path:
-            return expand_path(self.__data.get("path"))
-
-        def as_dict(self) -> dict:
-            exclude_keys = ["_Logging__data"]
-            return {
-                key: value
-                for key, value in self.__dict__.items()
-                if key not in exclude_keys
-            }
+    """Class for managing the configuration file."""
 
     def __init__(self) -> None:
-        match platform.system():
-            case "Linux":
-                self.__system = "linux"
-                self.__config_dir = expand_path(Path("~/.config")).joinpath("f_manager")
-            case "Darwin":
-                self.__system = "mac"
-                # TODO
-                # self.__config_dir = Path("~/.config")
-            case "Windows":
-                self.__system = "win"
-                # TODO
-                # self.__config_dir = Path("~/.config")
-            case _:
-                raise UnknownSystem
+        self.__system = self.get_system()
+        self.__config_dir = self.get_config_dir()
+        self.__sample_file = self.get_sample_file()
+        self.__file = self.get_config_file()
+        self.__parser = self.get_config_parser()
 
-        self.__sample_file = (
+    def get_system(self) -> str:
+        """Return the current system."""
+        system = platform.system()
+        systems = {
+            "Linux": "linux",
+            "Darwin": "mac",
+            "Windows": "win",
+        }
+
+        if s := systems.get(system):
+            return s
+
+        raise UnknownSystem
+
+    def get_config_dir(self) -> Path:
+        """Return the configuration directory."""
+        user_home_dirs = {
+            "linux": "~/.config",
+            "mac": "~/Library/Application Support",
+            "win": "%APPDATA%",
+        }
+
+        config_dir = expand_path(user_home_dirs[self.__system]).joinpath("f_manager")
+        config_dir.mkdir(parents=True, exist_ok=True)
+        return config_dir
+
+    def get_sample_file(self) -> Path:
+        """Return the sample configuration file."""
+        sample_file = (
             Path(__file__)
             .parent.joinpath("configs")
             .joinpath(f"config_{self.__system}.ini")
         )
-        if not self.__sample_file.exists():
+        if not sample_file.exists():
             raise FileNotFoundError(
-                f"""Can't find sample config file ({self.__sample_file}).
-Try reinstall the package or find config file manually in the project page"""
+                f"Can't find sample config file ({sample_file}). Try reinstalling the package or finding the config file manually on the project page"  # noqa: E501
             )
+        return sample_file
 
-        self.__file = self.__config_dir.joinpath("core.ini")
-        if not self.__file.exists():
-            os.makedirs(self.__config_dir, exist_ok=True)
-            shutil.copy(self.__sample_file, self.__file)
-        # self.__file = self.__sample_file
+    def get_config_file(self) -> Path:
+        """Return the configuration file."""
+        config_file = self.__config_dir.joinpath("core.ini")
+        if not config_file.exists():
+            shutil.copy(self.__sample_file, config_file)
+        return config_file
 
-        self.__parser = configparser.ConfigParser()
-        self.__parser.read(self.__file)
-
-    @cached_property
-    def logging(self) -> __Logging:
-        return self.__Logging(dict(self.__parser.items("logging")))
-
-    @cached_property
-    def data(self) -> __Data:
-        return self.__Data(dict(self.__parser.items("data")))
+    def get_config_parser(self) -> configparser.ConfigParser:
+        """Return the configuration parser."""
+        parser = configparser.ConfigParser()
+        parser.read(self.__file)
+        return parser
 
     @cached_property
-    def factorio(self) -> __Factorio:
-        return self.__Factorio(dict(self.__parser.items("factorio")))
+    def logging(self) -> LoggingConfigSection:
+        """Return the logging configuration section."""
+        return LoggingConfigSection(dict(self.__parser.items("logging")))
+
+    @cached_property
+    def data(self) -> DataConfigSection:
+        """Return the data configuration section."""
+        return DataConfigSection(dict(self.__parser.items("data")))
+
+    @cached_property
+    def factorio(self) -> FactorioConfigSection:
+        """Return the Factorio configuration section."""
+        return FactorioConfigSection(dict(self.__parser.items("factorio")))
 
     def update(self):
+        """Update the configuration file."""
         factorio_dict = self.factorio.as_dict()
         for key, value in factorio_dict.items():
             if value:
